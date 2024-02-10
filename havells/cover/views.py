498ac122f741a8c2579fb55009b608aece25ed52
Ashish <adhiman004@gmail.com>
2024-02-10 07:48:13 +0530

    need to seet this

--- havells/cover/views.py
+++ havells/cover/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from django.views import View
+from .models import Category
 # Create your views here.
 
 def home(request):
     return render(request,"app/home.html")
+
+class CategoryView(View):
+    def get(self,request,val):
+        products = Category.objects.filter(category=val)
+        # categoryid=Category.objects.filter(val=val)
+        return render(request,"app/category.html",locals())
